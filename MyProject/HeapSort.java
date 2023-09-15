a2e78ce8b768406606b8a0df5cc2c6306a0229eb
SergeyKaraman <karamans7997@gmail.com>
2023-09-15 14:31:27 +0300

    Урок 2. Структуры данных. Массивы. Алгоритмы массивов.
    Урок 2. Структуры данных. Массивы. Алгоритмы массивов.
    Реализовать алгоритм пирамидальной сортировки (сортировка кучей).

---
+++ MyProject/HeapSort.java
@@ -0,0 +1,60 @@
+public class HeapSort {
+    public static void main(String[] args) {
+        int[] arr = {12, 11, 13, 5, 6, 7};
+        heapSort(arr);
+        System.out.println("Отсортированный массив:");
+        printArray(arr);
+    }
+
+    public static void heapify(int[] arr, int n, int i) {
+        int largest = i;
+        int leftChild = 2 * i + 1;
+        int rightChild = 2 * i + 2;
+
+        if (leftChild < n && arr[leftChild] > arr[largest]) {
+            largest = leftChild;
+        }
+
+        if (rightChild < n && arr[rightChild] > arr[largest]) {
+            largest = rightChild;
+        }
+
+        if (largest != i) {
+            int temp = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = temp;
+            heapify(arr, n, largest);
+        }
+    }
+
+    public static void buildMaxHeap(int[] arr) {
+        int n = arr.length;
+        for (int i = n / 2 - 1; i >= 0; i--) {
+            heapify(arr, n, i);
+        }
+    }
+
+    public static void heapSort(int[] arr) {
+        int n = arr.length;
+
+        // Построение максимальной кучи поэлементно
+        for (int i = n / 2 - 1; i >= 0; i--) {
+            heapify(arr, n, i);
+        }
+
+        // Последовательное извлечение элементов и уменьшение кучи
+        for (int i = n - 1; i >= 0; i--) {
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+            heapify(arr, i, 0);
+        }
+    }
+
+    public static void printArray(int[] arr) {
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+}
